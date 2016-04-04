04d963e164633cf74305ea762f39f4c40fc89e58
Rafael Tejado <rtejado90@gmail.com>
2016-04-04 20:36:15 +0200

    Add changes from saturday

--- config.js
+++ config.js
@@ -3,6 +3,26 @@ var config = {};
 config.slack_token = "";
 config.slack_bot_name = "ypirqui";
 
+config.CHANNEL_INITIALS = ['C', 'G'];
+config.INSULTS = [
+  'cabron',
++ 'tonto',
++ 'tontopolla',
++ 'caraculo',
++ 'subnormal',
++ 'meapilas',
++ 'alcornoque',
++ 'cernicalo',
++ 'ceporro',
++ 'cenutrio',
++ 'culopollo',
++ 'gañan',
++ 'gorrumino',
++ 'gandul',
++ 'huevon',
++ 'lloramigas'
+];
+
 config.bot_random = 0.80;
 
 config.bot_min_string_length = 9;
